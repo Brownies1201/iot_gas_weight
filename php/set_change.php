417c85d95bd360ce6b7c1463751ba1e19cab86bd
Brownies1201 <brownies1201@gmail.com>
2016-01-12 17:22:55 +0800

    add the comment on set_change.php

--- php/set_change.php
+++ php/set_change.php
@@ -1,22 +1,22 @@
 <?php 
 header('Content-Transfer-Encoding: Binary');
-
+//db connection set
 $localhost = "localhost";
 $my_user = "test123";
 $my_password = "test123";
 $my_db =  "wifidb";
-
+//get the values from client
 $shop_name = $_POST["shop_name"];
 $shop_phone = $_POST["shop_phone"];
 $shop_address = $_POST["shop_address"];
-
+//db connection
 $con = mysqli_connect($localhost,$my_user,$my_password,$my_db);
 // Check connection
 if (mysqli_connect_errno())
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
 mysqli_set_charset($con,"utf8");
-
+//sql search
 $sql= "update user_contact set shop_name='".$shop_name."', phone='".$shop_phone."', address='".$shop_address."'";
 $result = mysqli_query($con,$sql);
 
